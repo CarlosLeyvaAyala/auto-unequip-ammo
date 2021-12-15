2076855047c4b5648f8236da2adef24b0e08b940
CarlosLeyvaAyala <64770846+CarlosLeyvaAyala@users.noreply.github.com>
2021-12-14 19:09:37 -0600

    Add reequip last used ammo

--- src/entry.ts
+++ src/entry.ts
@@ -1,6 +1,6 @@
 import { DebugLib as D } from "DmLib"
 import * as JDB from "JContainers/JDB"
-import * as StorageUtil from "PapyrusUtil/StorageUtil"
+import * as LibFire from "LibFire/LibFire"
 import {
   Actor,
   Ammo,
@@ -9,12 +9,18 @@ import {
   Game,
   on,
   printConsole,
-  SlotMask,
+  settings,
+  Utility,
   Weapon,
+  WeaponType,
 } from "skyrimPlatform"
 
 /** Internal name */
-const logLvl = D.Log.LevelFromSettings("auto-unequip-ammo", "loggingLevel")
+const n = "auto-unequip-ammo"
+
+const rememberAmmo = settings[n]["rememberAmmo"] as boolean | false
+const equipDelay = settings[n]["equipDelay"] as number | 0.1
+const logLvl = D.Log.LevelFromSettings(n, "loggingLevel")
 printConsole(`Auto unequip ammo logging level: ${D.Log.Level[logLvl]}`)
 
 // Generates a logging function specific to this mod.
@@ -45,78 +51,43 @@ const LogV = CLF(D.Log.Level.verbose)
 /** Logs a variable while initializing it. Message level: verbose. */
 const LogVT = D.Log.Tap(LogV)
 
+const key = ".auto-unequip"
+const keyB = key + ".bow"
+const keyC = key + ".crossbow"
+
 export function main() {
   printConsole("Auto unequip ammo successfully initalized")
 
-  /** Current ammo FormId.
-   * @Remarks
-   * Object id needs to be saved instead of the Ammo itself because object pointers in Skyrim Platform
-   * expire after some time.
-   */
-  let currentAmmo: number
-
-  /** A function that accepts a player object, an item and a weapon type and returns nothing. */
-  type DoSomethingFunc = (
-    player: Actor,
-    item: Form,
-    weaponType: WeaponType
-  ) => void
-
-  /**
-   * Does something when player (un)equips an item.
-   * @param logMsg Message to log to console.
-   * @param e Event variable that contains the data to work with
-   * @param DoSomething Function to execute once all data is gathered.
-   * @returns void
-   */
-  function StartAction(
-    logMsg: string,
-    e: EquipEvent,
-    DoSomething: DoSomethingFunc
-  ) {
-    // Exit if actor isn't player
-    const pl = Game.getPlayer()
-    if (e.actor.getFormID() !== pl?.getFormID()) return
-
-    // Get (un)equipped object type
-    const item = e.baseObj
-    LogV(`${logMsg}: ${item.getName()}`)
-    const t = LogVT("Type is", GetWeaponType(item), (t) => WeaponType[t])
-
-    DoSomething(pl, item, t)
-  }
-
-  const key = ".auto-uneqip"
-
-  /** Log quiver movement. */
-  const LogQ = (msg: string, ammo: Form) => {
-    LogI(`${msg}: ${D.Log.IntToHex(ammo.getFormID())}`)
-  }
-
   on("equip", (e) => {
     StartAction("Object equipped", e, (_, i, t) => {
-      if (t !== WeaponType.ammo) return
-      LogQ("Remembering equipped ammo", i)
-      JDB.solveFormSetter(key, i, true)
+      if (!(IsRanged(t) && rememberAmmo)) return
+      RestoreAmmo(t)
     })
   })
 
+  const IsRanged = (t: WAType) => t === WAType.bow || t === WAType.crossbow
+
   on("unequip", (e) => {
     StartAction("Object unequipped", e, (p, _, t) => {
-      if (t === WeaponType.bow || t === WeaponType.crossbow) {
-        const c = JDB.solveForm(key)
-        if (!c) {
-          LogE("Current ammo was lost. Did you modify esp files between saves?")
-          return
-        }
-        LogQ("Removing remembered ammo", c)
-        p.unequipItem(c, false, true)
-      }
+      if (!IsRanged(t)) return
+
+      const a = LibFire.GetEquippedAmmo(Game.getPlayer())
+      if (!a) return
+
+      if (rememberAmmo) RememberAmmo(a, t)
+      LogQ("Removing ammo", a)
+      p.unequipItem(a, false, true)
     })
   })
 }
 
-enum WeaponType {
+/** Log quiver movement. */
+const LogQ = (msg: string, ammo: Form) => {
+  LogI(`${msg}: ${D.Log.IntToHex(ammo.getFormID())}`)
+}
+
+/** Weapon/Ammo type. */
+enum WAType {
   none,
   bow,
   crossbow,
@@ -126,8 +97,67 @@ enum WeaponType {
 
 function GetWeaponType(item: Form) {
   const w = Weapon.from(item)
-  if (!w) return Ammo.from(item) ? WeaponType.ammo : WeaponType.none
-  if (w.getWeaponType() == 7) return WeaponType.bow
-  if (w.getWeaponType() == 9) return WeaponType.crossbow
-  return WeaponType.other
+  if (!w) return Ammo.from(item) ? WAType.ammo : WAType.none
+  if (w.getWeaponType() == WeaponType.Bow) return WAType.bow
+  if (w.getWeaponType() == WeaponType.Crossbow) return WAType.crossbow
+  return WAType.other
+}
+
+const GetJcKey = (w: WAType) => (w === WAType.bow ? keyB : keyC)
+
+const RememberAmmo = (a: Ammo, w: WAType) => {
+  LogQ("Remembering equipped ammo for later restoration", a)
+  const k = GetJcKey(w)
+  JDB.solveFormSetter(k, a, true)
+}
+
+const RestoreAmmo = (w: WAType) => {
+  const k = GetJcKey(w)
+  const i = JDB.solveForm(k)
+  if (!i) return
+  LogQ("Restoring remembered ammo", i)
+
+  Wait(i, equipDelay ? 0.1 : equipDelay, (x) => {
+    const a = Ammo.from(x)
+    if (!a) return
+    Game.getPlayer()?.equipItem(a, false, true)
+  })
+}
+
+export function Wait(o: Form, time: number, DoSomething: (a: Form) => void) {
+  const id = o.getFormID()
+  const f = async () => {
+    await Utility.wait(time)
+    const a = Game.getFormEx(id)
+    if (!a) return
+    DoSomething(a)
+  }
+  f()
+}
+
+/** A function that accepts a player object, an item and a weapon type and returns nothing. */
+type DoSomethingFunc = (player: Actor, item: Form, weaponType: WAType) => void
+
+/**
+ * Does something when player (un)equips an item.
+ * @param logMsg Message to log to console.
+ * @param e Event variable that contains the data to work with
+ * @param DoSomething Function to execute once all data is gathered.
+ * @returns void
+ */
+function StartAction(
+  logMsg: string,
+  e: EquipEvent,
+  DoSomething: DoSomethingFunc
+) {
+  // Exit if actor isn't player
+  const pl = Game.getPlayer()
+  if (e.actor.getFormID() !== pl?.getFormID()) return
+
+  // Get (un)equipped object type
+  const item = e.baseObj
+  LogV(`${logMsg}: ${item.getName()}`)
+  const t = LogVT("Type is", GetWeaponType(item), (t) => WAType[t])
+
+  DoSomething(pl, item, t)
 }
